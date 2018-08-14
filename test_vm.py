ef0c9585568ddf2fe2e21abc3715d9d12d0414e0
Yewon <jyw0754@gmail.com>
2018-08-14 15:17:04 +0900

    동전넣고 잔액확인하기

--- test_vm.py
+++ test_vm.py
@@ -1,2 +1,16 @@
-def test_plus() :
-    assert 3 == 1 + 2
+from vm import run, init
+
+def test_initial_chang_should_be_zero():
+    init()
+    assert "잔액은 0원입니다" == run("잔액")
+
+def test_insert_coin_and_check():
+    init()
+    assert "100원을 넣었습니다" == run("동전 100")
+    assert "잔액은 100원 입니다" == run("잔액")
+
+def test_insert_coin_and_check():
+    init()
+    run("동전 100")
+    run("동전 100")
+    assert "잔액은 200원입니다" == run("잔액")
