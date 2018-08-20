fb5019c3270e16b50d3c80dfd5e62f107fb2bc61
Yewon <jyw0754@gmail.com>
2018-08-20 12:36:48 +0900

    동전 제약하기

--- test_vm.py
+++ test_vm.py
@@ -3,37 +3,48 @@ from vm import Vendingmachine
 # 잔액 테스트
 def test_initial_chang_should_be_zero():
     m = Vendingmachine()
-    assert "잔액은 0원입니다." == m.run("잔액")
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 # 동전을 넣고 잔액이 올라가는지 확인
 def test_insert_coin_and_check():
     m = Vendingmachine()
-    assert "100원을 넣었습니다." == m.run("동전 100")
-    assert "잔액은 100원입니다." == m.run("잔액")
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 # 동전을 두 번 넣고 잔액이 올라가는지 확인
 def test_insert_coin_and_check():
     m = Vendingmachine()
     m.run("동전 100")
     m.run("동전 100")
-    assert "잔액은 200원입니다." == m.run("잔액")
+    assert "잔액은 200원입니다" == m.run("잔액")
 
 # 정의되지 않은 명령(몰라)을 넣었을 때 "알 수 없는 명령입니다."
 def test_error():
     m = Vendingmachine()
-    assert "알 수 없는 명령입니다." == m.run("몰라")
+    assert "알 수 없는 명령입니다" == m.run("몰라")
 
 # 커피는 150원
 # 잔액이 충분할 때 "커피가 나왔습니다."
 def test_order_coffee():
     m = Vendingmachine()
-    m.run("동전 100")
-    m.run("동전 100")
-    assert "커피가 나왔습니다." == m.run("음료 커피")
-    assert "잔액은 50원입니다." == m.run("잔액")
+    m.run("동전 500")
+    assert "커피가 나왔습니다" == m.run("음료 커피")
+    assert "잔액은 350원입니다" == m.run("잔액")
 
 # 잔액이 부족할 때 "잔액이 부족합니다."
 def test_order_coffee_error():
     m = Vendingmachine()
-    assert "잔액이 부족합니다." == m.run("커피")
-    assert "잔액은 0원입니다." == m.run("잔액")
+    m.run("동전 100")
+    assert "잔액이 부족합니다" == m.run("음료 커피")
+    assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_unknown_beverage_oder():
+    m = Vendingmachine()
+    m.run("동전 500")
+    assert "알 수 없는 음료입니다" == m.run("음료 맥주")
+    assert "잔액은 500원입니다" == m.run("잔액")
+
+def test_coin():
+    m = Vendingmachine()
+    m.run("동전 30")
+    assert "알 수 없는 동전입니다" == m.run("동전 30")
--- vm.py
+++ vm.py
@@ -11,24 +11,28 @@ class Vendingmachine:
         cmd, params = tokens[0], tokens[1:]
 
         if cmd == "잔액":
-            return "잔액은 " + str(self._change) + "원입니다."
+            return "잔액은 " + str(self._change) + "원입니다"
 
         elif cmd == "동전":
             coin = params[0]
-            self._change += int(coin)
-            return coin + "원을 넣었습니다."
+            if int(coin) not in [10, 50, 100, 500]:
+                return "알 수 없는 동전입니다"
+            else:
+                self._change += int(coin)
+                return coin + "원을 넣었습니다"
 
 # 커피를 입력하고 잔액이 150원이상일 때 커피값을 빼고 "커피가 나왔습니다."
-        elif cmd == "음료" and self._change >= 150:
-            coffee = 150
-            beverage = params[0]
-            self._change -= int(coffee)
-            return beverage + "가 나왔습니다."
+        elif cmd == "음료":
+            known_beverage = "커피"
+            price = 150
 
-# 커피를 입력하고 잔액이 150원 미만일 때 "잔액이 부족합니다."
-        elif cmd == "커피" and self._change < 150:
-            return "잔액이 부족합니다."
+            beverage = params[0]
+            if beverage != known_beverage:
+                return "알 수 없는 음료입니다"
+            if self._change < price:
+                return "잔액이 부족합니다"
+            self._change = self._change - price
+            return beverage + "가 나왔습니다"
 
         else:
-            return "알 수 없는 명령입니다."
-            pass
+            return "알 수 없는 명령입니다"
