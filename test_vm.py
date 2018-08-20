e3ab8460ae83f8f5723bc7bccf43bcf72858e661
Yewon <jyw0754@gmail.com>
2018-08-20 12:04:16 +0900

    음료 뽑기

--- test_vm.py
+++ test_vm.py
@@ -3,22 +3,37 @@ from vm import Vendingmachine
 # 잔액 테스트
 def test_initial_chang_should_be_zero():
     m = Vendingmachine()
-    assert "잔액은 0원입니다" == m.run("잔액")
+    assert "잔액은 0원입니다." == m.run("잔액")
 
 # 동전을 넣고 잔액이 올라가는지 확인
 def test_insert_coin_and_check():
     m = Vendingmachine()
-    assert "100원을 넣었습니다" == m.run("동전 100")
-    assert "잔액은 100원 입니다" == m.run("잔액")
+    assert "100원을 넣었습니다." == m.run("동전 100")
+    assert "잔액은 100원입니다." == m.run("잔액")
 
 # 동전을 두 번 넣고 잔액이 올라가는지 확인
 def test_insert_coin_and_check():
     m = Vendingmachine()
     m.run("동전 100")
     m.run("동전 100")
-    assert "잔액은 200원입니다" == m.run("잔액")
+    assert "잔액은 200원입니다." == m.run("잔액")
 
 # 정의되지 않은 명령(몰라)을 넣었을 때 "알 수 없는 명령입니다."
 def test_error():
     m = Vendingmachine()
-    assert "알 수 없는 명령입니다" == m.run("몰라")
+    assert "알 수 없는 명령입니다." == m.run("몰라")
+
+# 커피는 150원
+# 잔액이 충분할 때 "커피가 나왔습니다."
+def test_order_coffee():
+    m = Vendingmachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "커피가 나왔습니다." == m.run("음료 커피")
+    assert "잔액은 50원입니다." == m.run("잔액")
+
+# 잔액이 부족할 때 "잔액이 부족합니다."
+def test_order_coffee_error():
+    m = Vendingmachine()
+    assert "잔액이 부족합니다." == m.run("커피")
+    assert "잔액은 0원입니다." == m.run("잔액")
--- vm.py
+++ vm.py
@@ -1,18 +1,34 @@
 # Vendingmachine 클래스 생성
 class Vendingmachine:
+# self상태: 잔액을 0으로 초기화
     def __init__(self):
         self._change = 0
 
+# run메서드(연산): self, 문자열에서 공백으로 토큰을 나누고 토큰의 첫번째는 cmd로,
+# 두번째는 params로 정의
     def run(self, raw):
         tokens = raw.split(" ")
         cmd, params = tokens[0], tokens[1:]
 
         if cmd == "잔액":
-            return "잔액은 " + str(self._change) + "원입니다"
+            return "잔액은 " + str(self._change) + "원입니다."
+
         elif cmd == "동전":
             coin = params[0]
             self._change += int(coin)
-            return coin + "원을 넣었습니다"
+            return coin + "원을 넣었습니다."
+
+# 커피를 입력하고 잔액이 150원이상일 때 커피값을 빼고 "커피가 나왔습니다."
+        elif cmd == "음료" and self._change >= 150:
+            coffee = 150
+            beverage = params[0]
+            self._change -= int(coffee)
+            return beverage + "가 나왔습니다."
+
+# 커피를 입력하고 잔액이 150원 미만일 때 "잔액이 부족합니다."
+        elif cmd == "커피" and self._change < 150:
+            return "잔액이 부족합니다."
+
         else:
-            return "알 수 없는 명령입니다"
+            return "알 수 없는 명령입니다."
             pass
